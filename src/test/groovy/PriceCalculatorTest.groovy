1a6c7527bcda2a081f7fcdfa4a8d32cff294f04f
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2019-06-08 10:05:11 +0200

    include progress from conference

--- src/test/groovy/PriceCalculatorTest.groovy
+++ src/test/groovy/PriceCalculatorTest.groovy
@@ -1,22 +1,51 @@
 import spock.lang.Specification
 
-class PriceCalculatorTest extends Specification {
-    void setup() {}
-
-    def "should be non null"() {
-        when:
-        def testedObject = new PriceCalculator()
-        then:
-        testedObject != null
-    }
-}
-
-class PriceCalculator {
-    /*
+/*
+Implement class which will calculate prices.
 Prices:
 apple -> 20
 beer -> 200
 beer duo pack cost (15% discount)
 if sum > 500 then 10% of additional discount
 */
+
+class PriceCalculatorTest extends Specification {
+    private PriceCalculator testedObject
+
+    void setup() {
+        testedObject = new PriceCalculator()
+    }
+
+    def "should return 0 when 0 items added"() {
+        when:
+        def result = testedObject.calculate()
+
+        then:
+        result == 0
+    }
+
+    def "should return correct value for params"(){
+        expect:
+        testedObject.add(listOfItems)
+        result == testedObject.calculate()
+
+        where:
+        listOfItems | result
+        List.of()   | 0
+        List.of("apple")   | 20
+        List.of("apple", "apple")   | 40
+    }
+}
+
+class PriceCalculator {
+
+    private List<String> items = List.of()
+
+    void add(List<String> items) {
+        this.items = items
+    }
+
+    int calculate() {
+        return items.size() * 20
+    }
 }
